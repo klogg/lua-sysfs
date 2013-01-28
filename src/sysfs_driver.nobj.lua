c93ca71122409da0d2969709d4455adf92222964
Christian Wiese <chris@opensde.org>
2013-01-28 15:51:09 +0100

    add sysfs driver object

---
+++ src/sysfs_driver.nobj.lua
@@ -0,0 +1,45 @@
+-- Copyright (c) 2013 by Christian Wiese <chris@opensde.org>
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+
+--
+-- sysfs driver
+--
+
+-- typedefs
+local typedefs = [[
+typedef struct sysfs_driver driver;
+]]
+c_source "typedefs" (typedefs)
+-- pass extra C type info to FFI.
+ffi_cdef (typedefs)
+
+object "driver" {
+	-- open
+	constructor "open" {
+		c_call "driver *" "sysfs_open_driver" {
+						"const char *", "bus_name",
+						"const char *", "drv_name"
+		}
+	},
+	-- close
+	destructor "close" {
+		c_method_call "void" "sysfs_close_driver" {}
+	},
+}
