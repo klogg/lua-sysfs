6f98ce3d3ed059f386acc8946cb048e763de5d4e
Christian Wiese <chris@opensde.org>
2013-01-31 11:55:10 +0100

    device: change lua_sysfs_device_iterator() to use lua_sysfs_iterator_run macro

--- src/sysfs_device.nobj.lua
+++ src/sysfs_device.nobj.lua
@@ -33,24 +33,9 @@ ffi_cdef (typedefs)
 -- internal functions
 c_source "src" {
 [[
-
 /* internal sysfs device iterator function */
 static int lua_sysfs_device_iterator(lua_State *L) {
-	struct dlist *list = (struct dlist *) lua_touserdata(L, lua_upvalueindex(1));
-	struct sysfs_device *obj;
-
-	/* TODO: clarify the flag types
-	 * OBJ_UDATA_FLAG_OWN segfaults here with lua and luajit
-	 * OBJ_UDATA_FLAG_LOOKUP works with lua but segfaults with luajit
-	 */
-	int obj_flags = 0;
-
-	if ((obj = dlist_next(list)) != NULL) {
-		obj_type_sysfs_device_push(L, obj, obj_flags);
-		return 1;
-	}
-
-	return 0;
+	lua_sysfs_iterator_run(sysfs_device, obj_type_sysfs_device_push);
 }
 ]]
 }
